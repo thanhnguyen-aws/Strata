8d667664a7c97201cfd08562f3979fb2943139d4
thanhnguyen-aws <ntson@amazon.com>
2025-12-05 08:49:12 -0800

    add copyright

--- Strata/Languages/Python/BoogieTypePrelude.lean
+++ Strata/Languages/Python/BoogieTypePrelude.lean
@@ -1,3 +1,9 @@
+/-
+  Copyright Strata Contributors
+
+  SPDX-License-Identifier: Apache-2.0 OR MIT
+-/
+
 import Strata.DDM.Elab
 import Strata.DDM.AST
 import Strata.Languages.Boogie.DDMTransform.Parse
