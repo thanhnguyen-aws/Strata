e328a48264d341d07fc69a3b4348c875989307ab
Remy Willems <rwillems@amazon.com>
2025-12-17 16:27:03 +0100

    Move examples from `Strata` to `StrataTest` to reduce build time (#274)
    Before: `lake build 468.95s user 168.55s system 285% cpu 3:42.94 total`,
    413 jobs
    After: `lake build 422.01s user 119.11s system 300% cpu 2:59.78 total`,
    360 jobs
    
    `lake test` covers the moved example files
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.
    
    ---------
    
    Co-authored-by: Juneyoung Lee <136006969+aqjune-aws@users.noreply.github.com>

--- Strata.lean
+++ Strata.lean
@@ -16,15 +16,7 @@ import Strata.DL.Lambda.Lambda
 import Strata.DL.Imperative.Imperative
 
 /- Boogie -/
-import Strata.Languages.Boogie.Examples.Examples
 import Strata.Languages.Boogie.StatementSemantics
-
-/- CSimp -/
-import Strata.Languages.C_Simp.Examples.Examples
-
-/- Dyn -/
-import Strata.Languages.Dyn.Examples.Examples
-
 
 /- Code Transforms -/
 import Strata.Transform.CallElimCorrect
