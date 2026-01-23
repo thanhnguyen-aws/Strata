e85f517a03ef444335e5961610fda7c10e2bcc21
Andrew Wells <130512013+andrewmwells-amazon@users.noreply.github.com>
2026-01-23 12:01:58 -0600

    Tests for Python Class Declarations (#339)
    Tests for Python Class Declarations
    
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.

---
+++ StrataTest/Languages/Python/tests/test_class_decl.py
@@ -0,0 +1,14 @@
+import test_helper
+from typing import List
+
+class CircularBuffer:
+    def __init__(self, n: int):
+        print("Hi")
+
+def main():
+    my_buf: CircularBuffer = CircularBuffer(5)
+
+    print("Bye")
+
+if __name__ == "__main__":
+    main()
