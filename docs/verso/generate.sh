29a2b47471c8a2b5a87e5735a7ffeed4e989e2b9
Josh Cohen <36058610+joscoh@users.noreply.github.com>
2026-01-28 14:25:51 -0500

    Add API docs to Strata website (#353)
    *Description of changes:*
    Adds API docs to the Strata github pages site
    Update dependencies for API generation and CI
    
    Note that this does not include the API docs for StrataTest, since that
    requires running all the tests and introduces issues with solver
    dependencies in CI, etc. For now, only the Strata API is included.
    
    
    By submitting this pull request, I confirm that you can use, modify,
    copy, and redistribute this contribution, under the terms of your
    choice.
    
    ---------
    
    Co-authored-by: Josh Cohen <cohenjo@amazon.com>
    Co-authored-by: Aaron Tomb <aarotomb@amazon.com>
    Co-authored-by: Juneyoung Lee <136006969+aqjune-aws@users.noreply.github.com>
    Co-authored-by: Juneyoung Lee <lebjuney@amazon.com>
    Co-authored-by: Andrew Wells <130512013+andrewmwells-amazon@users.noreply.github.com>

--- docs/verso/generate.sh
+++ docs/verso/generate.sh
@@ -1,5 +1,11 @@
 set -ex
 
+curpwd=$(pwd)
+cd ../api
+lake build Strata:docs
+
+cd "${curpwd}"
 lake exe ddm --with-html-single --output _out/ddm
 lake exe langdef --with-html-single --output _out/langdef
 cp strata-hourglass.png _out/langdef/html-single/
+cp -r ../api/.lake/build/doc _out/api
