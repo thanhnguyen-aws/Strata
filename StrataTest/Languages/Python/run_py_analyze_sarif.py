5e8b9e67f464d58a353279d089e7606649306c13
thanhnguyen-aws <ntson@amazon.com>
2026-03-17 12:59:44 -0700

    merge conflict

--- StrataTest/Languages/Python/run_py_analyze_sarif.py
+++ StrataTest/Languages/Python/run_py_analyze_sarif.py
@@ -20,8 +20,7 @@ TEST_FILES = sorted(
 )
 
 BOTH_SKIP = {"test_foo_client_folder", "test_invalid_client_type", "test_unsupported_config"}
-SKIP_TESTS = BOTH_SKIP | {"test_class_field_use", "test_list", "test_subscription", "test_func_input_type_constraints"}
-SKIP_TESTS = BOTH_SKIP | {"test_class_field_use", "test_list", "test_subscription", "test_with_statement"}
+SKIP_TESTS = BOTH_SKIP | {"test_class_field_use", "test_list", "test_subscription", "test_with_statement", "test_func_input_type_constraints"}
 SKIP_TESTS_LAUREL = BOTH_SKIP
 
 
