e17f4414022fa534dccd07d7d34ace93d2848bfe
thanhnguyen-aws <ntson@amazon.com>
2026-05-08 15:51:35 -0700

    Update StrataTest/Languages/Python/tests/test_list_append_assign.py
    Co-authored-by: Michael Tautschnig <mt@debian.org>

--- StrataTest/Languages/Python/tests/test_list_append_assign.py
+++ StrataTest/Languages/Python/tests/test_list_append_assign.py
@@ -2,4 +2,9 @@ def test():
     xs = [1, 2, 3]
     y = xs.append(4)
     assert xs == [1, 2, 3, 4]
-    assert y == None  
+def test():
+    xs = [1, 2, 3]
+    y = xs.append(4)
+    assert xs == [1, 2, 3, 4]
+    assert y == None
+test()
